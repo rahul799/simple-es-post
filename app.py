540e35fae2d7924cfad38427cec7ebb445e22ca2
rahul799 <rahulagrawal799110@gmail.com>
2022-11-08 20:40:43 +0530

    tested

--- app.py
+++ app.py
@@ -5,12 +5,17 @@ f = open('data.json')
 
 data = json.load(f)
 
-url = "http://localhost:9200/your_index/_doc"
+url = "http://localhost:9200/my-index-000001/_doc"
+
+headers = {
+    'Content-Type': 'application/json',
+}
 
 count = 1
 for i in data["values"]:
     print(i)
-    req = requests.post(url,data=i)
+    req = requests.post(url + "/" + str(count),data=json.dumps(i), headers=headers)
+    count = count + 1
     print(req.text)
     print("")
     print(req.json)
